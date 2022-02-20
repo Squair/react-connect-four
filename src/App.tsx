ee3a55ec9be006c6e8be91a14e87b54a568dd81f
Squair <Georgesquair@gmail.com>
2022-02-20 10:29:53 +0000

    ✨ Add waiting lobby while searching for game

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
 import './App.css';
+import Lobby from './components/Lobby';
 import PreLobby from './components/PreLobby';
 
 const App = () => {
   const [username, setUsername] = useState<string>();
+  const [findingGame, setFindingGame] = useState<boolean>(false);
+
+  const beginFindingGame = () => setFindingGame(true);
+  const cancelFindingGame = () => setFindingGame(false);
 
   const onStartClick = () => {
     // Emit to socket.io, join queue
+    if (!username) return;
+
+    beginFindingGame();
+
   }
 
   return (
     <div className="App" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-      <PreLobby username={username} setUsername={setUsername} onStartClick={onStartClick} />
+      {!findingGame && <PreLobby username={username} setUsername={setUsername} onStartClick={onStartClick} /> }
+      {findingGame && username && <Lobby username={username} cancelFindingGame={cancelFindingGame} />}
     </div>
   );
 }
---
+++ src/components/Lobby.tsx
@@ -0,0 +1,21 @@
+import { Box, Button, CircularProgress, Typography } from "@mui/material";
+
+interface ILobbyProps {
+    username: string;
+    cancelFindingGame: () => void;
+}
+
+const Lobby = ({ username, cancelFindingGame }: ILobbyProps) => {
+    return (
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Typography variant='h2'>{username}</Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: '17.5em' }}>
+                <Typography variant='h5'>Searching for a game </Typography>
+                <CircularProgress />
+            </Box>
+            <Button variant='contained' onClick={cancelFindingGame}>Cancel</Button>
+        </Box>
+    )
+}
+
+export default Lobby;
