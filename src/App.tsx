7a6954f7f7e095f117649eb5d205258a2fc1f61f
Squair <Georgesquair@gmail.com>
2022-02-20 09:37:00 +0000

    ✨ Add username input with start button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Box, Button, Container } from '@mui/material';
+import React, { useState } from 'react';
 import './App.css';
+import UsernameInput from './components/UsernameInput';
 
-function App() {
+const App = () => {
+  const [username, setUsername] = useState<string>();
+
+  const onStartClick = () => {
+    // Emit to socket.io, join queue
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', width: '25%' }}>
+        <UsernameInput username={username} setUsername={setUsername} />
+        <Button variant='contained' onClick={onStartClick}>Find Game</Button>
+      </Box>
     </div>
   );
 }
---
+++ src/components/UsernameInput.tsx
@@ -0,0 +1,13 @@
+import { TextField } from "@mui/material";
+
+interface IUsernameInput {
+    username: string | undefined;
+    setUsername: React.Dispatch<React.SetStateAction<string | undefined>>;
+}
+
+const UsernameInput = ({ username, setUsername }: IUsernameInput) => {
+    const onUsernameChange = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => setUsername(e.target.value);
+    return <TextField value={username} onChange={onUsernameChange} placeholder='Enter username...' />
+}
+
+export default UsernameInput;
