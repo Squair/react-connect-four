c8c7a4f62b830d398d1ff877ccbb74ceb7e9d5bb
Squair <Georgesquair@gmail.com>
2022-02-20 15:18:07 +0000

    ✨ Add play counter to names above grid

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -40,13 +40,17 @@ const App = () => {
     beginFindingGame();
   }
 
+  const player = foundGame?.players.filter(x => x.id === socket?.id)[0];
+  const opposingPlayer = foundGame?.players.filter(x => x.id !== socket?.id)[0];
+
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
       {!findingGame && !foundGame && <PreLobby username={username} setUsername={setUsername} onStartClick={onStartClick} />}
       {findingGame && username && <Lobby username={username} cancelFindingGame={stopFindingGame} />}
+
       {foundGame && socket && foundGame && (
         <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <Typography textAlign='center' variant='h3'>{username} vs {foundGame.players.filter(x => x.username !== username)[0].username}</Typography>
+          <Typography textAlign='center' variant='h3'>{player?.username}({player?.counter}) vs {opposingPlayer?.username}({opposingPlayer?.counter})</Typography>
           <ConnectFourGrid socket={socket} game={foundGame} columns={7} rows={6} winningContiguousCounters={4} />
         </div>
       )}
