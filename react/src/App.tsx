602169fdbdd945a39d9e67bb094cae69ca352686
Squair <Georgesquair@gmail.com>
2022-03-04 19:37:55 +0000

    📃 Style change to make easier to read

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -12,7 +12,16 @@ const App = () => {
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
       {gameState === 'Idle' && <PreLobby username={username} setUsername={setUsername} onStartClick={beginFindingGame} />}
       {gameState === 'Finding game' && username && <Lobby username={username} cancelFindingGame={endGame} />}
-      {gameState === 'Found game' && foundGame && socket && <ConnectFour socket={socket} game={foundGame} finishGame={endGame} columns={7} rows={6} contiguousCountersToWin={4} />}
+
+      {gameState === 'Found game' && foundGame && socket && (
+        <ConnectFour socket={socket}
+          game={foundGame}
+          finishGame={endGame}
+          columns={5}
+          rows={4}
+          contiguousCountersToWin={4}
+        />
+      )}
     </div>
   );
 }
