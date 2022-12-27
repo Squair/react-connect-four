55459cfcb049a09f3dc2cfba259f53f79de804da
Squair <georgesquair@gmail.com>
2022-12-27 16:12:18 +0000

    Fix mui errors

--- react/src/components/PreLobby.tsx
+++ react/src/components/PreLobby.tsx
@@ -1,5 +1,4 @@
-import { Button, Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import { Box, Button, Typography } from "@mui/material";
 import { IGameboardSize } from "../interface/IGameboardSize";
 import BoardSizeSelector from "./BoardSizeSelector";
 import UsernameInput from "./UsernameInput";
